f93c48d1540841949ee018a56510afe7c7d8fd59
Tathya Garg <tathya.garg@gmail.com>
2024-01-15 02:03:26 +0530

    sum_of_squares.s works now. Thankfully I figured it out on my own and didn't have to copy from the book

--- sum_of_squares.s
+++ sum_of_squares.s
@@ -1,24 +1,40 @@
-section .text
-    global _start
-    _start:
 ; The equivalent code in C:
 ; =========================
 ; int main() {
-;     int MAXIMUM = 20;
+;     int maximum = 5;
 ;     int total = 0;
-;     for (int i=MAXMIMUM, i>0,i--) {
-;         total += i*i;
-;      }
+;     int helper = 0; // Represents the value in the RAX register
+;     summation(&helper, &total, &maximum);
+;     printf("%d\n", total); // Debug statement
+;     finish();
 ; }
+; void summation(int *helper, int *total, int *maximum) {
+;     *helper = *maximum;
+;     *helper = *helper * *helper;
+;     *total += *helper;
+;     *maximum -= 1;
+;     if (*maximum == 0) return;
+;     summation(helper, total, maximum);
+; }
+; void finish() { /*  Random exiting code */ }
 ; =========================
-        mov ax, 0 ; Will help hold i^2
-        mov cl, 5 ; i
-    sumLoop:
-        movzx ax, cl
-        mul ax
-        add bx, ax
-        loop sumLoop
-    last:
-        mov rax, 1 ; Mark as success
-        mov rdi, 60
-        syscall ; Exit
+
+section .data
+    SUCCESS equ 0
+    SYS_exit equ 60
+    maximum dq 5
+    total dq 0
+
+section .text
+    global _start
+    _start:
+        mov rcx, [maximum]
+    summation:
+        mov rax, rcx
+        mul rax
+        add [total], rax
+        loop summation
+    finish:
+        mov rax, SUCCESS
+        mov rdi, SYS_exit
+        syscall
