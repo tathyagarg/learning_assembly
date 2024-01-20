886904525e2a1c6f187fe87e4d1cacaf345e5211
Tathya Garg <tathya.garg@gmail.com>
2024-01-20 15:27:11 +0530

    Rewritten int_to_string.s to be more efficient

--- int_to_string.s
+++ int_to_string.s
@@ -1,65 +1,35 @@
-; Program to convert an integer into a string (more like a list)
-; The debugger script to verify proper execution is:
-; =============================================
-; b debugger
-; run
-; set logging file out.txt
-; set logging overwrite
-; set logging on
-; echo -----------------------\n
-; while (1)
-;     stepi
-;     print $rax
-;     c
-; end
-; echo \n\n
-; set logging off
-; quit
-; =============================================
-
 section .data
-    SUCCESS equ 0
+    NULL equ 0
     SYS_EXIT equ 60
+    SUCCESS equ 0
 
-    DEBUG dq 1
-    number dq 1379  ; Number to convert to a null-terminated string
-    digit_count dq 4
+    number dq 1928190
 section .bss
-    stringed resq 10
+    stringNumber resb 10
 section .text
-    global _start
-preDivideLoop:
-    mov [number], rax
-    jmp divideLoop
+global _start
 _start:
+    mov rcx, 0
+    mov rsi, 0
     mov rbx, 10
-    mov rsi, 1
 divideLoop:
-    mov rax, qword [number]
+    mov rax, [number]
     mov rdx, 0
     div rbx
-    push rdx  ; Pushes the digits from the right into stack
-    cmp qword [number], 0
-    je prePopLoop  ; Exits the loop if number is exhausted
-    inc rsi
-    jmp preDivideLoop
+    inc rcx
+    mov [number], rax
+    push rdx
+    cmp rax, 0
+    jne divideLoop
 prePopLoop:
-    mov rcx, rsi
-    mov rsi, 0
-    pop rax ; Remove trailing something idk
+    mov rbx, stringNumber
 popLoop:
-    pop rax ; Move left-most digit into rax
-    mov [stringed+rsi*8], rax  ; Move rax into the left-most digit of the string
+    pop rax
+    add al, "0" ; No idea why
+    mov byte [rbx+rsi], al
     inc rsi
     loop popLoop
-    cmp qword [DEBUG], 0
-    je exit  ; Exit if we don't want to debug
-    mov rcx, qword [digit_count]  ; Continue from here if we're going to debug
-    mov rsi, 0
-debugger:
-    mov rax, [stringed+rsi*8]
-    inc rsi
-    loop debugger
+    mov byte [rbx+rsi], NULL
 exit:
     mov rax, SYS_EXIT
     mov rdi, SUCCESS
